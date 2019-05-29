4b50873b808e5f5e40cd60aaebadd30e5d62aaca
Janas <janas@java.com>
2019-05-29 21:47:35 +0200

    JM_1: Variables,Input,Output

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class FirstClass {
     public static void main(String[] args) {
+
+        int userValue;
+        String user;
+
+        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide your lucky number: ");
+        userValue = sc.nextInt();
+        System.out.println("Great! Your lucky number is: " + userValue + "\n");
+
+        System.out.println("What is your name?");
+        user = sc.next();
+        System.out.println("Hello " + user);
 
     }
 }
